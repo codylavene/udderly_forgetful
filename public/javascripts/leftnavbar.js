045453f90a191fac2442796a64e4a2d890d19a4a
justin chau <jchau623@gmail.com>
2021-12-15 13:23:45 -0500

    css struggles

---
+++ public/javascripts/leftnavbar.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const todayButton = document.querySelector('#today');
+
+
+    todayButton.addEventListener('click', e => {
+        todayButton.classList.add('red');
+    })
+})
