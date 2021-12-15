f91fd9e4beb6c0f6cfc9f88c63c582257cb84101
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2021-12-15 10:26:00 -0600

    landing page stylings in progress, images rendering, working on carousel

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -2,16 +2,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const swiperImg = document.querySelector(".swiper-img");
   const swiperHead = document.querySelector(".swiper-head");
   const swiperText = document.querySelector(".swiper-text");
-  const imgs = [
-    "../assets/images/hp_steve_1.png",
-    "../assets/images/hp_steve_2.png",
-    "../assets/images/hp_steve_3.png",
-    "../assets/images/hp_steve_4.png",
-  ];
+
   let i = 0;
-  const slide = () => {
-    swiperImg.style.backgroundImg = imgs[i];
-    i++;
+  const swiper = () => {
+    swiperImg.classList.add(".active");
+    swiperImg.classList.add(".prev");
+    swiperImg.classList.add(".prev");
   };
-  setInterval(slide, 600);
+  setTimeout(swiper, 600);
 });
