515b6ec6b25a6b6aa8d17fd90e08b04e83e4528c
justin chau <jchau623@gmail.com>
2021-12-16 11:44:59 -0500

    initial

--- public/javascripts/leftnavbar.js
+++ public/javascripts/leftnavbar.js
@@ -1,50 +1,29 @@
+
 window.addEventListener("DOMContentLoaded", () => {
-  // const todayButton = document.querySelector('#today');
-  // todayButton.addEventListener('click', e => {
-  //     todayButton.style.color = '#d7e7f5'
-  // })
 
-  const allTaskButton = document.querySelector(".all-lists");
-  const todayButton = document.querySelector("#today");
-  const tomorrowButton = document.querySelector("#tomorrow");
-  const thisWeekButton = document.querySelector("#this-week");
-  const trashButton = document.querySelector("#trash");
+  const AllTasks = document.querySelector("all-tasks");
+  const hamburgerHelper = (e) => {
+    e.stopPropagation()
+    const todayButton = document.querySelector("#today");
+    todayButton.classList.toggle("height-transition");
+    const tomorrowButton = document.querySelector("#tomorrow");
+    tomorrowButton.classList.toggle("height-transition");
+    const thisWeekButton = document.querySelector('#this-week')
+    thisWeekButton.classList.toggle("height-transition");
+    const trashButton = document.querySelector("#trash");
+    trashButton.classList.toggle("height-transition");
+  };
+    lists.addEventListener('click', hamburgerHelper)
 
-  allTaskButton.addEventListener("click", (e) => {
-    allTaskButton.appendChild(todayButton);
-    allTaskButton.appendChild(tomorrowButton);
-    allTaskButton.appendChild(thisWeekButton);
-    allTaskButton.appendChild(trashButton);
-    if (allTaskButton.children.length === 4) {
-      // allTaskButton.innerText = "Expand List"
-      allTaskButton.removeChild(todayButton);
-      allTaskButton.removeChild(tomorrowButton);
-      allTaskButton.removeChild(thisWeekButton);
-      allTaskButton.removeChild(trashButton);
-      // allTaskButton.removeEventListener()
-    }
-      allTaskButton.addEventListener("click", (e) => {
-        if(allTaskButton.children.length === 0) {
-        allTaskButton.innerText = "All Tasks";
-        allTaskButton.appendChild(todayButton);
-        allTaskButton.appendChild(tomorrowButton);
-        allTaskButton.appendChild(thisWeekButton);
-        allTaskButton.appendChild(trashButton);
-          }
-      });
-  });
-
-
-
-
-  const listsButton = document.querySelector('.added-lists')
-  listsButton.addEventListener('click', e => {
-      for( let i = 0; i <= listsButton.children.length; i++) {
-          let listChildren = listsButton.children[i]
-          listsButton.removeChild(listChildren)
-      }
-      if (!listChildren) {
-          listsButton.appendChild()
-      }
-  })
+  // const addedListButton = document.querySelector('lists')
+  // const cheeseburgerHelper = (e) => {
+  //   if (e.target !== list) {
+  //     e.stopPropagation()
+  //   }
+  //   const children = document.querySelectorAll('.added-list-children');
+  //   children.forEach(child => {
+  //     child.classList.toggle("height-transition");
+  //   })
+  // };
+  // addedListButton.addEventListener('click', cheeseburgerHelper)
 });
