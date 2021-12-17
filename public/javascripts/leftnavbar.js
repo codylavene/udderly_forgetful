b30956b49b5b1cdd0220a6028d825f495f566a36
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2021-12-17 12:32:19 -0600

    adding tasks to list and database

--- public/javascripts/leftnavbar.js
+++ public/javascripts/leftnavbar.js
@@ -1,30 +1,27 @@
-
 window.addEventListener("DOMContentLoaded", () => {
-
   const allTasks = document.querySelector("#all-tasks");
   const taskCollapse = (e) => {
     const todayButton = document.querySelector("#today");
     todayButton.classList.toggle("height-transition");
     const tomorrowButton = document.querySelector("#tomorrow");
     tomorrowButton.classList.toggle("height-transition");
-    const thisWeekButton = document.querySelector('#this-week')
+    const thisWeekButton = document.querySelector("#this-week");
     thisWeekButton.classList.toggle("height-transition");
     const trashButton = document.querySelector("#trash");
     trashButton.classList.toggle("height-transition");
-  }
-  allTasks.addEventListener('click', taskCollapse)
+  };
+  allTasks.addEventListener("click", taskCollapse);
 
-  const addedListButton = document.querySelector('#list')
+  const addedListButton = document.querySelector(".list");
   const listCollapse = (e) => {
-    const children = document.querySelectorAll('.added-list-children');
-    console.log(e.target.className)
-    if (e.target.className === 'added-list-children') {
+    const children = document.querySelectorAll(".added-list-children");
+    // console.log(e.target.className);
+    if (e.target.className === "added-list-children") {
       return;
     }
-    children.forEach(child => {
+    children.forEach((child) => {
       child.classList.toggle("height-transition");
-    })
-  }
-  addedListButton.addEventListener('click', listCollapse)
-
+    });
+  };
+  addedListButton.addEventListener("click", listCollapse);
 });
--- public/javascripts/user-content.js
+++ public/javascripts/user-content.js
@@ -1,24 +1,35 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   const addTaskInput = document.getElementById("add-task-input");
   const addTaskButton = document.getElementById("add-task-button");
-
+  const addedTasks = document.querySelector(".added-tasks");
+  const lists = document.querySelector(".added-lists");
+  let listId;
+  /*--------------------------------------------------------------------*/
   addTaskInput.addEventListener("focus", (e) => {
     addTaskButton.classList.remove("hide-button");
   });
-
+  /*--------------------------------------------------------------------*/
+  lists.addEventListener("click", (e) => {
+    listId = e.target.id;
+  });
+  /*--------------------------------------------------------------------*/
   document.addEventListener("click", (e) => {
     if (e.target !== addTaskInput) {
       addTaskButton.classList.add("hide-button");
     }
   });
-  addTaskButton.addEventListener("click", (e) => {
-    e.preventDefault();
+  /*--------------------------------------------------------------------*/
+  addTaskButton.addEventListener("click", async (e) => {
+    e.target.preventDefault();
     const val = addTaskInput.value;
     const div = document.createElement("div");
-    div.appendChild();
     div.classList.add("filled");
-    const boiler = `<input type="checkbox>
-                    <p> ${val}`;
+    const boiler = `<input type="checkbox"><p> ${val} </p>`;
     div.innerHTML = boiler;
+    addedTasks.appendChild(div);
+    addTaskInput.value = "";
+    // let description = val;
+    // const task = await db.Task.create({ description });
+    // await fetch("/api/tasks");
   });
 });
