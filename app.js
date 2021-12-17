e493dea4b8a29534a4c94462ab026d34aa3a6c09
justin chau <jchau623@gmail.com>
2021-12-16 19:04:22 -0500

    tours almost done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const listsRouter = require('./routes/lists')
+const toursRouter = require('./routes/tours')
 const { sessionSecret, environment } = require('./config')
 
 
@@ -41,6 +42,7 @@ store.sync();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(listsRouter);
+app.use('/tour', toursRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/tour.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { environment } = require("../config");
-const db = require("../db/models");
-const { asyncHandler } = require("./utils");
-
-router.get("/tour", asyncHandler(async (req, res, next) => {
-    res.send("OK");
-  }));
