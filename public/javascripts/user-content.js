34befaf11f8f04d1972e7d0a8c40a3d521569650
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2021-12-16 19:26:25 -0600

    fixed pug bug

--- public/javascripts/user-content.js
+++ public/javascripts/user-content.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   const addTaskInput = document.getElementById("add-task-input");
   const addTaskButton = document.getElementById("add-task-button");
-  const taskContent = document.addTaskInput.addEventListener("focus", (e) => {
+
+  addTaskInput.addEventListener("focus", (e) => {
     addTaskButton.classList.remove("hide-button");
   });
 
@@ -10,14 +11,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
       addTaskButton.classList.add("hide-button");
     }
   });
-  const boiler = `<input type="checkbox>
-                  <p> ${val}`;
   addTaskButton.addEventListener("click", (e) => {
     e.preventDefault();
     const val = addTaskInput.value;
     const div = document.createElement("div");
     div.appendChild();
     div.classList.add("filled");
+    const boiler = `<input type="checkbox>
+                    <p> ${val}`;
     div.innerHTML = boiler;
   });
 });
