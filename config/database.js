72bce944087ca77911ab3a0e57320f9a51858491
justin chau <jchau623@gmail.com>
2021-12-21 16:22:49 -0500

    database url reverted

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'postgres://ppljxifixnwhgj:866ee9eb42d9b7c7f0ebb4b2b46ba851589b86e6c6642e3aafe7ae67dcc42265@ec2-3-230-149-158.compute-1.amazonaws.com:5432/d6ru5s4ml325it',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
