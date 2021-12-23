5807bfa10cc46b80210255e88516bc1b7461025b
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2021-12-23 11:27:18 -0600

    Carousel is functioning

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -2,12 +2,45 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const swiperImg = document.querySelector(".swiper-img");
   const swiperHead = document.querySelector(".swiper-head");
   const swiperText = document.querySelector(".swiper-text");
-
-  let i = 0;
-  const swiper = () => {
-    swiperImg.classList.add(".active");
-    swiperImg.classList.add(".prev");
-    swiperImg.classList.add(".prev");
+  let index = 1;
+  let timer = setInterval(() => {
+    changeSlide(1);
+  }, 5000);
+  const changeSlide = (n) => {
+    // plus slides
+    clearInterval(timer);
+    if (n < 0) {
+      showSlide((index -= 1));
+    } else {
+      showSlide((index += 1));
+    }
+    if (n === -1) {
+      timer = setInterval(() => {
+        changeSlide(n + 2);
+      }, 5000);
+    } else {
+      timer = setInterval(() => {
+        changeSlide(n + 1);
+      }, 5000);
+    }
   };
-  setTimeout(swiper, 600);
+
+  const activeSlide = (n) => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      changeSlide(n + 1);
+    }, 5000);
+    showSlide((index = n));
+  };
+
+  const showSlide = (n) => {
+    const slides = document.getElementsByClassName("slide");
+    if (n > slides.length) index = 1;
+    if (n < 1) index = slides.length;
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[index - 1].style.display = "block";
+  };
+  showSlide(index);
 });
